801e046b8090a8cb1ecb25f95077370015e3235d
Ankit-Pandey1 <ankit93156@gmail.com>
2024-07-31 09:03:39 +0530

    New Changes

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,9 +1,13 @@
 'use strict';
 
-// Selecting elements
-const player0El = document.querySelector('.player--0');
-const player1El = document.querySelector('.player--1');
+//Selecting elements
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const current0El = document.getElementById('current--0');
-const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+
+//starting conditions
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
