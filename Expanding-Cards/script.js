4977f81830de988d5fa154ba476b410b20f5787e
Ankit-Pandey1 <ankit93156@gmail.com>
2024-08-06 21:05:19 +0530

    Practice Project

---
+++ Expanding-Cards/script.js
@@ -0,0 +1,17 @@
+
+
+const panels = document.querySelectorAll('.panel')
+
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeAvtiveClasses()
+    panel.classList.add('active')
+  })
+})
+
+function removeAvtiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active')
+  })
+}
