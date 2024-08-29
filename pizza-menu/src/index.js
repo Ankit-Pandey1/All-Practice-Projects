34822a12002f4c3b3421d5b93677f5302141faf1
Ankit-Pandey1 <ankit93156@gmail.com>
2024-08-29 23:13:31 +0530

    react practice

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,73 +1,115 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const pizzaData = [
   {
-    name: "Focaccia",
-    ingredients: "Bread with italian olive oil and rosemary",
+    name: 'Focaccia',
+    ingredients: 'Bread with italian olive oil and rosemary',
     price: 6,
-    photoName: "pizzas/focaccia.jpg",
+    photoName: 'pizzas/focaccia.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Margherita",
-    ingredients: "Tomato and mozarella",
+    name: 'Pizza Margherita',
+    ingredients: 'Tomato and mozarella',
     price: 10,
-    photoName: "pizzas/margherita.jpg",
+    photoName: 'pizzas/margherita.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Spinaci",
-    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    name: 'Pizza Spinaci',
+    ingredients: 'Tomato, mozarella, spinach, and ricotta cheese',
     price: 12,
-    photoName: "pizzas/spinaci.jpg",
+    photoName: 'pizzas/spinaci.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Funghi",
-    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    name: 'Pizza Funghi',
+    ingredients: 'Tomato, mozarella, mushrooms, and onion',
     price: 12,
-    photoName: "pizzas/funghi.jpg",
+    photoName: 'pizzas/funghi.jpg',
     soldOut: false,
   },
   {
-    name: "Pizza Salamino",
-    ingredients: "Tomato, mozarella, and pepperoni",
+    name: 'Pizza Salamino',
+    ingredients: 'Tomato, mozarella, and pepperoni',
     price: 15,
-    photoName: "pizzas/salamino.jpg",
+    photoName: 'pizzas/salamino.jpg',
     soldOut: true,
   },
   {
-    name: "Pizza Prosciutto",
-    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    name: 'Pizza Prosciutto',
+    ingredients: 'Tomato, mozarella, ham, aragula, and burrata cheese',
     price: 18,
-    photoName: "pizzas/prosciutto.jpg",
+    photoName: 'pizzas/prosciutto.jpg',
     soldOut: false,
   },
 ];
 
 function App() {
   return (
-    <div>
-      <h1>Hello React!!!!</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
+}
+
+function Header() {
+  // const style = { color: 'red', fontSize: '48px', textTransform: 'uppercase' };
+  const style = {};
+
+  return (
+    <header className="header footer">
+      <h1 style={style}>Fast React Pizza Co.</h1>
+    </header>
+  );
+}
+
+function Menu() {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </main>
+  );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 21;
+  const isOpen = hour >= openHour && hour <= closeHour;
+  console.log(isOpen);
+
+  // if (hour >= openHour && hour <= closeHour) alert("We're currently open!");
+  // else alert("Sorry we're closed");
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleDateString()}. We're currently open
+    </footer>
+  );
+
+  // return React.createElement('footer', null, "we're currently open!");
 }
 
 function Pizza() {
   return (
     <div>
       <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
+      <h3>Pizza Spinaci</h3>
       <p>Tomato, mozarella, spinach, and ricotta cheese</p>
     </div>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
