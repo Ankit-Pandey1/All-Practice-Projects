d8c3a01f1f0d56044f2370b3418fcc46e4edc4cf
Ankit Pandey <76793996+Ankit-Pandey1@users.noreply.github.com>
2024-12-19 23:12:35 +0530

    Delete LMS directory

--- LMS/server/controllers/user.controller.js
+++
@@ -1,6 +0,0 @@
-const register = (req, res) => {};
-const login = (req, res) => {};
-const logout = (req, res) => {};
-const getProfile = (req, res) => {};
-
-export { register, login, logout, getProfile };
--- LMS/server/server.js
+++
@@ -1,9 +0,0 @@
-import app from './app.js';
-import connectionToDB from './config/dbConnection.js';
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, async () => {
-  connectionToDB();
-  console.log(`App is running at http:localhost:${PORT}`);
-});
